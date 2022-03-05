000265d1f83e4e5e9b81cd667f85f2fd5f346e1c
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-05 23:41:04 +0100

    working on error management

--- src/so_long.c
+++ src/so_long.c
@@ -3,8 +3,11 @@
 int	main(int argc, char	argv[])
 {
 	if (argc != 2)
-		return (0);
-	
+		end(1, "Invalid number of arguments");
+	// if (!valid_map(argv[1]))
+	// 	end(1, "Invalid map");
+
+	return (0);
 }
 
 
