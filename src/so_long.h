a56d9d6e93799d976c264e663fe9323ea63aae0e
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-05 22:18:14 +0100

    header added

---
+++ src/so_long.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/05 22:15:32 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/03/05 22:17:43 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+#include "../mlx_linux/mlx.h"
+#include <stdio.h>
+
+// #define WIDTH 1080
+// #define HEIGHT 720
+
+#define WIDTH 720
+#define HEIGHT 480
+
+#endif
