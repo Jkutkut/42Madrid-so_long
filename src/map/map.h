f6873bfe7ef6202198519450a22a1df51783b394
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-07 11:03:06 +0100

    stable

--- src/map/map.h
+++ src/map/map.h
@@ -6,16 +6,21 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 12:04:05 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/06 12:33:38 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/07 11:01:17 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAP_H
 # define MAP_H
 
-#include "../get_next_line/get_next_line.h"
+#include "../so_long.h"
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 1024
+#endif
 
 int		endswith(char *str, char *end);
 void	check_map_filename(char *filename);
+char	**load_map(char *filename);
 
 #endif
