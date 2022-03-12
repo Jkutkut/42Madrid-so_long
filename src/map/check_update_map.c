66956eea3448a05f06d02e90dcdfea5a4b6fd19b
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-12 12:50:58 +0100

    Working on map verification. Border ready

--- src/map/check_update_map.c
+++ src/map/check_update_map.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 11:22:01 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/10 13:37:27 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/12 12:50:23 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,14 @@ void	check_update_map(t_map *m)
 	while (m->map[i])
 	{
 		lineLen = ft_strlen(m->map[i]);
+		printf("%s\n", m->map[i]);
 		if (lineLen != m->width)
 			end(1, "Invalid map:\n Map width is not constant.");
 		if (m->map[i][0] != '1' || m->map[i][lineLen - 1] != '1')
-			end(1, "Invalid map:\n All rows must start and end with 1.");
+			end(1, "Invalid map:\n The map must be surronded by 1s.");
+		i++;
 	}
+	while (lineLen--)
+		if (m->map[0][lineLen] != '1' || m->map[i - 1][lineLen] != '1')
+			end(1, "Invalid map:\n The map must be surronded by 1s.");
 }
--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 11:52:34 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/11 09:26:45 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/12 11:20:36 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,69 +17,69 @@
 // 	system("leaks -q so_long");
 // }
 
-// int	main(int argc, char	*argv[])
+int	main(int argc, char	*argv[])
+{
+	
+	if (argc != 2)
+		end(1, "Invalid number of arguments");
+	// atexit(check_leaks);
+	t_map *map = create_map(load_map(argv[1]));
+	check_update_map(map);
+	
+	int i = 0;
+	while (map->map[i])
+		printf("-%s-\n", map->map[i++]);
+	if (map->map != NULL)
+		freearray(map->map);
+	free(map);
+	return (0);
+}
+
+
+
+
+
+// typedef struct	s_data {
+// 	void	*img;
+// 	char	*addr;
+// 	int		bits_per_pixel;
+// 	int		line_length;
+// 	int		endian;
+// }				t_data;
+
+
+// void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 // {
-	
-// 	if (argc != 2)
-// 		end(1, "Invalid number of arguments");
-// 	// atexit(check_leaks);
-// 	t_map *map = create_map(load_map(argv[1]));
-// 	check_update_map(map);
-	
-// 	int i = 0;
-// 	while (map->map[i])
-// 		printf("-%s-\n", map->map[i++]);
-// 	if (map->map != NULL)
-// 		freearray(map->map);
-// 	free(map);
-// 	return (0);
+// 	char	*dst;
+
+// 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+// 	*(unsigned int*)dst = color;
 // }
 
+// int	main(void)
+// {
+// 	void	*mlx;
+// 	void	*mlx_win;
+// 	t_data	img;
 
-
-
-
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
-
-int	main(void)
-{
-	void	*mlx;
-	void	*mlx_win;
-	t_data	img;
-
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "so_long");
+// 	mlx = mlx_init();
+// 	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "so_long");
 	
-	img.img = mlx_new_image(mlx, WIDTH / 2, HEIGHT / 2);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+// 	img.img = mlx_new_image(mlx, WIDTH / 2, HEIGHT / 2);
+// 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 	
-	for (int i = 0; i < WIDTH / 2; i++)
-		for (int j = 0; j < HEIGHT / 2; j++)
-			my_mlx_pixel_put(&img, i, j, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+// 	for (int i = 0; i < WIDTH / 2; i++)
+// 		for (int j = 0; j < HEIGHT / 2; j++)
+// 			my_mlx_pixel_put(&img, i, j, 0x00FF0000);
+// 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 
-	int w, h;
-	void	*image = mlx_xpm_file_to_image(mlx_win, "res/Cat Sprite Sheet.png", &w, &h);
-	if (image != NULL)
-		mlx_put_image_to_window(mlx, mlx_win, image, 0, 0);
-	else
-		printf("Error: Could not load image\n");
-	printf("%d %d\n", w, h);
+// 	int w, h;
+// 	void	*image = mlx_xpm_file_to_image(mlx_win, "res/Cat Sprite Sheet.png", &w, &h);
+// 	if (image != NULL)
+// 		mlx_put_image_to_window(mlx, mlx_win, image, 0, 0);
+// 	else
+// 		printf("Error: Could not load image\n");
+// 	printf("%d %d\n", w, h);
 
-	mlx_loop(mlx);
-}
+// 	mlx_loop(mlx);
+// }
