29a978c53136801d20dd3167fc346a014d157e95
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-13 13:25:04 +0100

    all sprites loaded!

--- src/game/create_game.c
+++ src/game/create_game.c
@@ -6,14 +6,13 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 13:34:44 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/13 12:38:46 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/13 13:23:19 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 
-#define P_ANI 8
-#define E_ANI 4
+
 
 static t_img	*load_xpm(t_game *game, char *path)
 {
@@ -30,7 +29,7 @@ static void	load_player(t_game *game)
 	char	path[] = "./res/cat/cat00.xpm";
 	int		i;
 
-	game->imgplayer = malloc(sizeof(t_player *) * (P_ANI + 1));
+	game->imgplayer = malloc(sizeof(t_player *) * (P_ANI));
 	if (game->imgplayer == NULL)
 		end(2, "Error: malloc failed");
 	i = 0;
@@ -40,7 +39,6 @@ static void	load_player(t_game *game)
 		path[14] = i % 10 + '0';
 		game->imgplayer[i - 1] = load_xpm(game, path);
 	}
-	game->imgplayer[i - 1] = NULL;
 }
 
 static void	load_enemy(t_game *game)
@@ -48,7 +46,7 @@ static void	load_enemy(t_game *game)
 	char	path[] = "./res/enemy/enemy00.xpm";
 	int		i;
 
-	game->imgenemy = malloc(sizeof(t_player *) * (E_ANI + 1));
+	game->imgenemy = malloc(sizeof(t_player *) * (E_ANI));
 	if (game->imgenemy == NULL)
 		end(2, "Error: malloc failed");
 	i = 0;
@@ -58,7 +56,32 @@ static void	load_enemy(t_game *game)
 		path[18] = i % 10 + '0';
 		game->imgenemy[i - 1] = load_xpm(game, path);
 	}
-	game->imgenemy[i - 1] = NULL;
+}
+
+static void	load_exit(t_game *game)
+{
+	game->imgexit = malloc(sizeof(t_img *) * (2));
+	if (game->imgexit == NULL)
+		end(2, "Error: malloc failed");
+	game->imgexit[EXIT_O] = load_xpm(game, EXIT_OPEN_SRC);
+	game->imgexit[EXIT_C] = load_xpm(game, EXIT_CLOSED_SRC);
+}
+
+static void	load_env(t_game *game)
+{
+	game->imgenv = malloc(sizeof(t_player *) * (ENV_SRCS));
+	if (game->imgenv == NULL)
+		end(2, "Error: malloc failed");
+	game->imgenv[ENV_FLOOR] = load_xpm(game, ENV_FLOOR_SRC);
+	game->imgenv[ENV_L_T] = load_xpm(game, ENV_L_T_SRC);
+	game->imgenv[ENV_L_L] = load_xpm(game, ENV_L_L_SRC);
+	game->imgenv[ENV_L_R] = load_xpm(game, ENV_L_R_SRC);
+	game->imgenv[ENV_L_B] = load_xpm(game, ENV_L_B_SRC);
+	game->imgenv[ENV_C_TL] = load_xpm(game, ENV_C_TL_SRC);
+	game->imgenv[ENV_C_TR] = load_xpm(game, ENV_C_TR_SRC);
+	game->imgenv[ENV_C_BL] = load_xpm(game, ENV_C_BL_SRC);
+	game->imgenv[ENV_C_BR] = load_xpm(game, ENV_C_BR_SRC);
+	game->imgenv[ENV_WALL] = load_xpm(game, ENV_WALL_SRC);
 }
 
 t_game	*create_game(t_map *map)
@@ -77,5 +100,11 @@ t_game	*create_game(t_map *map)
 	load_player(game);
 	printf("Loading enemy\n");
 	load_enemy(game);
+	printf("Loading coin\n");
+	game->imgcoin = load_xpm(game, COIN_SRC);
+	printf("Loading exit\n");
+	load_exit(game);
+	printf("Loading wall and floor\n");
+	load_env(game);
 	return (game);
 }
--- src/game/game.h
+++ src/game/game.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 13:35:08 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/13 12:15:43 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/13 13:20:11 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,47 @@
 #ifndef HEIGHT
 # define HEIGHT 480
 #endif
+
+#define P_ANI 8
+#define E_ANI 4
+
+#ifndef COIN_SRC
+# define COIN_SRC "./res/env/C/key01.xpm"
+#endif
+
+#ifndef EXIT_CLOSED_SRC
+#define EXIT_CLOSED_SRC "./res/env/E/trapdoor02.xpm"
+#endif
+
+#ifndef EXIT_OPEN_SRC
+#define EXIT_OPEN_SRC "./res/env/E/stairs02.xpm"
+#endif
+
+#define EXIT_O 0
+#define EXIT_C 1
+
+#define ENV_SRCS 10
+#define ENV_FLOOR 0
+#define ENV_L_T 1
+#define ENV_L_L 2
+#define ENV_L_R 3
+#define ENV_L_B 4
+#define ENV_C_TL 5
+#define ENV_C_TR 6
+#define ENV_C_BL 7
+#define ENV_C_BR 8
+#define ENV_WALL 9
+
+#define ENV_FLOOR_SRC "./res/env/0/floor.xpm"
+#define ENV_L_T_SRC "./res/env/1/wall_line_top.xpm"
+#define ENV_L_L_SRC "./res/env/1/wall_line_left.xpm"
+#define ENV_L_R_SRC "./res/env/1/wall_line_right.xpm"
+#define ENV_L_B_SRC "./res/env/1/wall_line_bot.xpm"
+#define ENV_C_TL_SRC "./res/env/1/wall_corner_top_left.xpm"
+#define ENV_C_TR_SRC "./res/env/1/wall_corner_top_right.xpm"
+#define ENV_C_BL_SRC "./res/env/1/wall_corner_bot_left.xpm"
+#define ENV_C_BR_SRC "./res/env/1/wall_corner_bot_right.xpm"
+#define ENV_WALL_SRC "./res/env/1/wall.xpm"
 
 #include "../../mlx/mlx.h"
 #include "../../mlx/mlx_int.h"
@@ -41,10 +82,9 @@ typedef struct s_game
 	t_player	*player;
 	t_img		**imgplayer;
 	t_img		**imgenemy;
-	t_img		**imgcoin;
+	t_img		*imgcoin;
 	t_img		**imgexit;
-	t_img		**imgwall;
-	t_img		**imgfloor;
+	t_img		**imgenv;
 	void		*mlx;
 	void		*win;
 }				t_game;
