c9ce752d5607b12379c3847c0e2213410cb80a16
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-13 15:14:07 +0100

    Game loaded on screen

--- src/game/load_level.c
+++ src/game/load_level.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 13:50:38 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/13 14:08:38 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/13 15:10:44 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,15 +53,15 @@ void	load_level(t_game *game)
 		j = 1;
 		while (j < game->map->width - 1)
 		{
-			if (ft_hasany("0FC", game->map->map[i][j]))
+			if (game->map->map[i][j] == '0')
 				showimg(game->imgenv[ENV_FLOOR], j, i, game);
 			else if (game->map->map[i][j] == '1')
 				showimg(game->imgenv[ENV_WALL], j, i, game);
 			else if (game->map->map[i][j] == 'E')
 				showimg(game->imgexit[EXIT_C], j, i, game);
-			if (game->map->map[i][j] == 'C')
+			else if (game->map->map[i][j] == 'C')
 				showimg(game->imgcoin, j, i, game);
-			if (game->map->map[i][j] == 'P')
+			else if (game->map->map[i][j] == 'P')
 				showimg(game->imgplayer[0], j, i, game);
 			
 			j++;
