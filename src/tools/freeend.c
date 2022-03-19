77c9196a5b4c4cc401addb097153e6c7af99da4d
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-19 20:05:02 +0100

    double free error found

--- src/tools/freeend.c
+++ src/tools/freeend.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 17:14:13 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/19 19:35:21 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/19 20:01:02 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ void	freeend(int endtype, char *msg, t_game *game)
 	free_img_arr(game->imgenv, ENV_SRCS, game);
 	mlx_destroy_window(game->mlx, game->win);
 	mlx_destroy_display(game->mlx);
-	free(game->mlx);
+	// free(game->mlx);
 	printf("destroying game\n");
 	free(game);
 	end(endtype, msg);
