8adb967e4dd3f8ba7399072747015aa97005109e
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-19 20:12:38 +0100

    segfault fixed

--- src/tools/freeend.c
+++ src/tools/freeend.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 17:14:13 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/19 20:10:04 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/19 20:12:26 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,29 +23,21 @@ static void	free_img_arr(t_img **img, int len, t_game *game)
 	{
 		if (!img[i])
 			break;
-		mlx_destroy_image(game->mlx, img[i]);
-		// free(img[i++]);
+		mlx_destroy_image(game->mlx, img[i++]);
 	}
 	free(img);
 }
 
 void	freeend(int endtype, char *msg, t_game *game)
 {
-	printf("freeend\n");
 	freemap(game->map);
-	printf("player\n");
 	free_img_arr(game->imgplayer, P_ANI, game);
-	printf("enemy\n");
 	free_img_arr(game->imgenemy, E_ANI, game);
-	printf("coin\n");
 	mlx_destroy_image(game->mlx, game->imgcoin);
 	free_img_arr(game->imgexit, 2, game);
 	free_img_arr(game->imgenv, ENV_SRCS, game);
-	printf("mlx\n");
 	mlx_destroy_window(game->mlx, game->win);
 	mlx_destroy_display(game->mlx);
-	// free(game->mlx);
-	printf("destroying game\n");
 	free(game);
 	end(endtype, msg);
 }
