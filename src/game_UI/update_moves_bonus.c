c286f59a3428fdda731cd8d2da13178f54367217
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 19:02:20 +0100

    update_moves_bonus documented

--- src/game_UI/update_moves_bonus.c
+++ src/game_UI/update_moves_bonus.c
@@ -6,12 +6,17 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 11:35:23 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/20 18:53:51 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 19:02:03 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game_ui.h"
 
+/**
+ * @brief Shows on screen the number of moves made by the player.
+ * 
+ * @param game Game playing.
+ */
 void	update_moves(t_game *game)
 {
 	static int	moves = 0;
@@ -28,5 +33,5 @@ void	update_moves(t_game *game)
 	}
 	show_img(game->imgenv[ENV_C_TL], 0, 0, game);
 	show_cell(1, 0, game);
-	mlx_string_put(game->mlx, game->win, 10, 10, 0xFFFFFF, moves_str);
+	mlx_string_put(game->mlx, game->win, 10, 20, 0xFFFFFF, moves_str);
 }
