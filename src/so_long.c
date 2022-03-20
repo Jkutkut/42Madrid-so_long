e322e92dc7f9de7051aa8542b56399f63886bbda
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-20 13:20:30 +0100

    Norminette OK

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 11:52:34 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/20 12:51:33 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/20 13:20:18 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,19 @@
 // {
 // 	system("leaks -q so_long");
 // }
+// atexit(check_leaks);
 
 int	main(int argc, char	*argv[])
 {
-	
+	t_map	*map;
+	t_game	*game;
+
 	if (argc != 2)
 		end(1, "Invalid number of arguments");
-	// atexit(check_leaks);
-	t_map *map = create_map(load_map(argv[1]));
+	map = create_map(load_map(argv[1]));
 	check_update_map(map);
 	print_map(map);
-	t_game *game = create_game(map);
+	game = create_game(map);
 	show_level(game);
 	mlx_loop(game->mlx);
 	return (0);
