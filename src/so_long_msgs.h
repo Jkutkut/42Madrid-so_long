0331fc10f55a1c3d1d0dbd4428e7f5c46a695895
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-28 13:49:37 +0200

    Now all msgs/errors of file src/so_long_msgs.h from .h file

--- src/so_long_msgs.h
+++ src/so_long_msgs.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 13:04:45 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/28 13:17:39 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/28 13:47:21 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,18 @@
 # define ERROR_INVALID_ARGS "Invalid number of arguments.\n"
 # define ERROR_MALLOC "Malloc failed\n"
 # define ERROR_LOADING_IMG_DETAIL "Error loading image: "
-# define ERROR_LOADING_IMG "Error loading image\n."
-# define ERROR_ ""
-# define ERROR_ ""
+# define ERROR_LOADING_IMG "Error loading image.\n"
+# define ERROR_NO_MAP "No map file specified.\n"
+# define ERROR_MAP_EXTENSION "File should be a .ber file\n"
+# define ERROR_MULTIPLE_PLAYERS "Too many players.\n"
+# define ERROR_MAP_EMPTY "Invalid map.\n  Map empty.\n"
+# define ERROR_MAP_WIDTH "Invalid map:\n  Map width is not constant.\n"
+# define ERROR_MAP_BORDER "Invalid map:\n  The map must have border.\n"
+# define ERROR_MAP_CEP "Invalid map:\n  The map must contain CEP.\n"
+# define ERROR_MAP_INVALID_CHAR "Invalid map:\n  Invalid charactes in map.\n"
+# define ERROR_MAP_NULL "Invalid map:\n  Map is null.\n"
+# define ERROR_MAP_NOTFOUND "Map file not found.\n"
+# define ERROR_MAP_EMPTY "Map file is empty.\n"
 # define ERROR_ ""
 # define ERROR_ ""
 
