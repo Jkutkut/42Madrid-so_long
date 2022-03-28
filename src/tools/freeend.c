259147c324e95dc1effd1ebb6212f93e2fd2f098
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-28 13:54:48 +0200

    free_img_arr minor fix and freeend file documented

--- src/tools/freeend.c
+++ src/tools/freeend.c
@@ -6,12 +6,19 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 17:14:13 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/20 13:24:48 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/28 13:54:22 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tools.h"
 
+/**
+ * @brief Frees the memory allocated in the given img array.
+ * 
+ * @param img Image array to free.
+ * @param len Length of the array.
+ * @param game Game where imgs come from.
+ */
 static void	free_img_arr(t_img **img, int len, t_game *game)
 {
 	int	i;
@@ -19,15 +26,20 @@ static void	free_img_arr(t_img **img, int len, t_game *game)
 	if (!img)
 		return ;
 	i = 0;
-	while (i < len)
+	while (i < len && img[i])
 	{
-		if (!img[i])
-			break ;
 		mlx_destroy_image(game->mlx, img[i++]);
 	}
 	free(img);
 }
 
+/**
+ * @brief Frees all the allocated memory for the game and ends the execution.
+ * 
+ * @param endtype Type of ending (see end function).
+ * @param msg Message to send to end function.
+ * @param game Game to free.
+ */
 void	freeend(int endtype, char *msg, t_game *game)
 {
 	freemap(game->map);
