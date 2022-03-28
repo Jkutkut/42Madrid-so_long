79b89ea63c3d9735c6306a3dd804538d79ebde57
Jkutkut <jorgeregonzalez@gmail.com>
2022-03-28 13:49:37 +0200

    Now all msgs/errors of file src/tools/end.c from .h file

--- src/tools/end.c
+++ src/tools/end.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 23:31:18 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/07 12:00:25 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/03/28 13:47:40 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 void	end(int endtype, char *msg)
 {
 	if (endtype == 1)
-		ft_putstr_fd("Error: \n", 2);
+		ft_putstr_fd("Error:\n", 2);
 	ft_putstr_fd(msg, endtype + 1);
 	exit(endtype);
 }
