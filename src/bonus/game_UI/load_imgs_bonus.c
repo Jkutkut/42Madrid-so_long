09dfc205b03d10eb0087a22fa77adf12fbb37763
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-04 08:42:38 +0200

    load_imgs_bonus should be ready

--- src/bonus/game_UI/load_imgs_bonus.c
+++ src/bonus/game_UI/load_imgs_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 13:25:51 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/04 08:24:20 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/04 08:40:16 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,24 +55,24 @@ static void	load_players(t_game *game)
 	int		i;
 
 	ft_memcpy(path, PLAYER_SRC_BASE, ft_strlen(PLAYER_SRC_BASE) + 1);
-	game->imgplayer = malloc(sizeof(t_player *) * (P_ANI));
+	game->imgplayer = malloc(sizeof(void *) * (P_ANI));
 	if (game->imgplayer == NULL)
 		freeend(1, ERROR_MALLOC, game);
 	i = 0;
 	while (++i <= P_ANI)
 	{
 		path[ft_strlen(PLAYER_SRC_BASE) - ft_strlen(IMG_IND)] = i % 10 + '0';
-		game->imgplayer[i - 1] = load_xpm(game, path);
+		game->imgplayer[i - 1] = load_png(game, path);
 	}
 	ft_memcpy(path, ENEMY_SRC_BASE, ft_strlen(ENEMY_SRC_BASE) + 1);
-	game->imgenemy = malloc(sizeof(t_player *) * (E_ANI));
+	game->imgenemy = malloc(sizeof(void *) * (E_ANI));
 	if (game->imgenemy == NULL)
 		freeend(1, ERROR_MALLOC, game);
 	i = 0;
 	while (++i <= E_ANI)
 	{
 		path[ft_strlen(ENEMY_SRC_BASE) - ft_strlen(IMG_IND)] = i % 10 + '0';
-		game->imgenemy[i - 1] = load_xpm(game, path);
+		game->imgenemy[i - 1] = load_png(game, path);
 	}
 }
 
@@ -83,11 +83,11 @@ static void	load_players(t_game *game)
  */
 static void	load_exit(t_game *game)
 {
-	game->imgexit = malloc(sizeof(t_img *) * (2));
+	game->imgexit = malloc(sizeof(void *) * (2));
 	if (game->imgexit == NULL)
 		freeend(1, ERROR_MALLOC, game);
-	game->imgexit[EXIT_O] = load_xpm(game, EXIT_OPEN_SRC);
-	game->imgexit[EXIT_C] = load_xpm(game, EXIT_CLOSED_SRC);
+	game->imgexit[EXIT_O] = load_png(game, EXIT_OPEN_SRC);
+	game->imgexit[EXIT_C] = load_png(game, EXIT_CLOSED_SRC);
 }
 
 /**
@@ -98,19 +98,19 @@ static void	load_exit(t_game *game)
  */
 static void	load_env(t_game *game)
 {
-	game->imgenv = malloc(sizeof(t_player *) * (ENV_SRCS));
+	game->imgenv = malloc(sizeof(void *) * (ENV_SRCS));
 	if (game->imgenv == NULL)
 		freeend(1, ERROR_MALLOC, game);
-	game->imgenv[ENV_FLOOR] = load_xpm(game, ENV_FLOOR_SRC);
-	game->imgenv[ENV_L_T] = load_xpm(game, ENV_L_T_SRC);
-	game->imgenv[ENV_L_L] = load_xpm(game, ENV_L_L_SRC);
-	game->imgenv[ENV_L_R] = load_xpm(game, ENV_L_R_SRC);
-	game->imgenv[ENV_L_B] = load_xpm(game, ENV_L_B_SRC);
-	game->imgenv[ENV_C_TL] = load_xpm(game, ENV_C_TL_SRC);
-	game->imgenv[ENV_C_TR] = load_xpm(game, ENV_C_TR_SRC);
-	game->imgenv[ENV_C_BL] = load_xpm(game, ENV_C_BL_SRC);
-	game->imgenv[ENV_C_BR] = load_xpm(game, ENV_C_BR_SRC);
-	game->imgenv[ENV_WALL] = load_xpm(game, ENV_WALL_SRC);
+	game->imgenv[ENV_FLOOR] = load_png(game, ENV_FLOOR_SRC);
+	game->imgenv[ENV_L_T] = load_png(game, ENV_L_T_SRC);
+	game->imgenv[ENV_L_L] = load_png(game, ENV_L_L_SRC);
+	game->imgenv[ENV_L_R] = load_png(game, ENV_L_R_SRC);
+	game->imgenv[ENV_L_B] = load_png(game, ENV_L_B_SRC);
+	game->imgenv[ENV_C_TL] = load_png(game, ENV_C_TL_SRC);
+	game->imgenv[ENV_C_TR] = load_png(game, ENV_C_TR_SRC);
+	game->imgenv[ENV_C_BL] = load_png(game, ENV_C_BL_SRC);
+	game->imgenv[ENV_C_BR] = load_png(game, ENV_C_BR_SRC);
+	game->imgenv[ENV_WALL] = load_png(game, ENV_WALL_SRC);
 }
 
 /**
@@ -121,7 +121,7 @@ static void	load_env(t_game *game)
 void	load_imgs(t_game *game)
 {
 	load_players(game);
-	game->imgcoin = load_xpm(game, COIN_SRC);
+	game->imgcoin = load_png(game, COIN_SRC);
 	load_exit(game);
 	load_env(game);
 }
