bf8bf451dd82c194840883cf4ae5cc579ad38663
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-12 08:47:58 +0200

    let's fix this errors on the new version

--- scripts/test_invalid.sh
+++ scripts/test_invalid.sh
@@ -7,22 +7,43 @@ YELLOW='\033[1;33m';
 GREEN='\033[1;32m';
 BLUE='\033[1;34m';
 
-# make &&
-make bonus &&
-
-for i in $(ls ../maps/invalid/*.ber); do
-	f=$(basename $i | sed 's/\.ber//' | sed 's/invalid_//');
-	echo "$f: \c"
-
-	result=$(timeout -k9 1 ../so_long $i 2>&1)
-	isError=$(echo "$result" | grep "Error")
-	isTimeout=$(echo $result | cat -e)
-	if [ ! "$isError" = "" ]; then
-		echo "${GREEN}OK${NC}"
-	elif [ ! "$isTimeout" = "" ]; then
-		echo "${RED}timeout${NC}"
-	else
-		echo "${RED}KO${NC}"
-		break
+main() {
+	# Check timeout command exist
+	if [ ! -x "$(command -v timeout)" ]; then
+		echo "timeout command not installed/not found."
+		echo "You can install it with:"
+		echo "    brew install coreutils"
+		return
 	fi
-done
+
+	if [ "$1" = "bonus" ]; then
+		echo "Test invalid Bonus"
+		make bonus
+	else
+		echo "Test invalid"
+		make
+	fi &&
+
+	for i in $(ls maps/invalid/*.ber); do
+		f=$(basename $i | sed 's/\.ber//' | sed 's/invalid_//');
+		echo "$f: \c"
+
+		result=$(timeout -k9 1 ./so_long $i 2>&1)
+		isError=$(echo "$result" | grep "Error")
+		isTimeout=$(echo $result | cat -e)
+		if [ ! "$isError" = "" ]; then
+			echo "${GREEN}OK${NC}"
+		elif [ ! "$isTimeout" = "" ]; then
+			echo "${RED}timeout${NC}"
+		else
+			echo "${RED}KO${NC}"
+			break
+		fi
+	done ||
+	{
+		echo "${RED}Error:${NC}"
+		echo "    Not able to execute the test"
+	}
+}
+
+main $@
