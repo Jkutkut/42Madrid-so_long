4baf1c4e32533c35887a53869f92291468cddcbb
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-12 10:12:38 +0200

    bug at freemap fixed

--- src/bonus/tools/freemap_bonus.c
+++ src/bonus/tools/freemap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 13:08:45 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/03/31 15:19:39 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/12 10:11:49 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ void	freemap(t_map *m)
 {
 	int	i;
 
-	i = 0;
 	if (!m)
 		return ;
 	if (m->map)
 	{
-		while (i < m->height)
+		i = 0;
+		while (i < m->height && m->map[i])
 		{
 			free(m->map[i]);
 			i++;
