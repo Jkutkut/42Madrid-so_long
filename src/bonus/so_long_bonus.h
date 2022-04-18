7d94dd091baa735d9827c657fa71fdfda677ff11
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-18 20:15:24 +0200

    maxSize mac implemented

--- src/bonus/so_long_bonus.h
+++ src/bonus/so_long_bonus.h
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 22:15:32 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/10 17:30:00 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/18 20:13:07 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 // C libraries
 # include "../mlx/include/MLX42/MLX42.h"
 # define GAME_NAME "so_long Bonus"
-# define MAX_WIDTH 29
-# define MAX_HEIGHT 16
+# define MAX_WIDTH 42
+# define MAX_HEIGHT 20
 
 # include <stdio.h>
 # include <stdlib.h>
