64cbf5c4d31ba1ddcc36fda18b53507ce17551e6
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-19 15:23:17 +0200

    exits bug where they appear open fixed

--- src/bonus/game_UI/show_cell_bonus.c
+++ src/bonus/game_UI/show_cell_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/19 19:44:36 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/18 15:54:03 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/19 15:22:48 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,16 @@
  */
 void	show_cell(int x, int y, t_game *game)
 {
+	int	i;
+
 	if (game->map->map[y][x] == '0' || game->map->map[y][x] == 'C')
 		show_img(game->imgenv[ENV_FLOOR], x, y, game);
 	else if (game->map->map[y][x] == 'E')
 	{
 		show_img(game->imgexit[EXIT_O], x, y, game);
 		show_img(game->imgexit[EXIT_C], x, y, game);
-		game->imgexit[EXIT_C]->instances[0].z = 1;
+		i = game->imgexit[EXIT_C]->count - 1;
+		game->imgexit[EXIT_C]->instances[i].z = 1;
 	}
 	else if (game->map->map[y][x] == '1')
 		show_wall(game, x, y);
