fc14c5e62cb52d6ca96765a964fd150ca2d449eb
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-19 17:57:32 +0200

    .h updated to final version

--- src/bonus/so_long_bonus.h
+++ src/bonus/so_long_bonus.h
@@ -6,22 +6,27 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/05 22:15:32 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/18 20:13:07 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/19 17:56:37 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_BONUS_H
 # define SO_LONG_BONUS_H
 
-// C libraries
+// MLX
 # include "../mlx/include/MLX42/MLX42.h"
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+// Game constants
 # define GAME_NAME "so_long Bonus"
 # define MAX_WIDTH 42
 # define MAX_HEIGHT 20
 
-# include <stdio.h>
-# include <stdlib.h>
-
+// Libft
 # include "../libft/libft.h"
 
 // Declare and structures
@@ -29,6 +34,7 @@
 # include "so_long_structs_bonus.h"
 # include "so_long_msgs_bonus.h"
 
+// Code
 # include "game/game_bonus.h"
 # include "game_control/game_control_bonus.h"
 # include "game_UI/game_ui_bonus.h"
