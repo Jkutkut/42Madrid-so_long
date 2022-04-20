c1b2e41c48befa6dd73a20b04912913716cd19b5
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-20 12:00:20 +0200

    ready

--- scripts/install_homebrew.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh
--- scripts/install_mlx_mac.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-brew update &&
-brew install glfw &&
-echo "Ready! Just run make on the lib!" ||
-echo "Failed"
