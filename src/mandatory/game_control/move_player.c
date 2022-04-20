dc8e2f3fdc1441221cef991f70e62c1a323b2b40
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-20 10:51:45 +0200

    bug fixed

--- src/mandatory/game_control/move_player.c
+++ src/mandatory/game_control/move_player.c
@@ -6,7 +6,7 @@
 /*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 22:11:58 by jre-gonz          #+#    #+#             */
-/*   Updated: 2022/04/20 10:05:21 by jre-gonz         ###   ########.fr       */
+/*   Updated: 2022/04/20 10:50:06 by jre-gonz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,11 @@
  */
 void	move_player(int dx, int dy, t_game *game)
 {
-	int	i;
-
 	if (!move_cooldown(0) || !can_move_there(dx, dy, game))
 		return ;
 	game->player.x += dx;
 	game->player.y += dy;
-	update_moves(game);
+	update_moves();
 	game->imgplayer->instances[0].x += dx * IMG_SIZE;
 	game->imgplayer->instances[0].y += dy * IMG_SIZE;
 	if (game->map->map[game->player.y][game->player.x] == 'C')
