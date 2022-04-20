7dca61219a186781f4dd84406737311fbd8cf04c
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-20 09:40:46 +0200

    tools mandatory should be ready

---
+++ src/mandatory/tools/tools.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/07 11:56:33 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/04/20 09:24:05 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TOOLS_H
+# define TOOLS_H
+
+# include "../so_long.h"
+
+void	end(int endtype, char *msg);
+void	freearray(char **array);
+void	freeend(int endtype, char *msg, t_game *game);
+void	freemap(t_map *m);
+void	ft_strextend(char **s1, char *s2);
+void	print_map(t_map *m);
+
+#endif
