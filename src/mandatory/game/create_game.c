5f95fb33864fd20dde87fffbf481c36e322f5d5f
Jkutkut <jorgeregonzalez@gmail.com>
2022-04-20 09:47:07 +0200

    game ready for mandatory

---
+++ src/mandatory/game/create_game.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_game.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/12 13:34:44 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/04/20 09:42:20 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "game.h"
+
+/**
+ * @brief Create a game object form the given map.
+ * 
+ * @param map Map to create the game from.
+ * @return t_game* Game object.
+ */
+t_game	*create_game(t_map *map)
+{
+	t_game	*game;
+
+	game = (t_game *)malloc(sizeof(t_game));
+	if (!game)
+		return (NULL);
+	game->map = map;
+	game->coins = count_coins(map);
+	game->p_index = 0;
+	get_player(game);
+	game->mlx = mlx_init(map->width * IMG_SIZE, map->height * IMG_SIZE, \
+		GAME_NAME, false);
+	if (!game->mlx)
+		freeend(1, ERROR_MLX_INIT, game);
+	return (game);
+}
---
+++ src/mandatory/game/get_player.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_player.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jre-gonz <jre-gonz@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 20:19:15 by jre-gonz          #+#    #+#             */
+/*   Updated: 2022/04/20 09:43:12 by jre-gonz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "game.h"
+
+/**
+ * @brief Get the player form the map.
+ * 
+ * @param game Game structure.
+ */
+void	get_player(t_game *game)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < game->map->height)
+	{
+		j = 0;
+		while (j < game->map->width)
+		{
+			if (game->map->map[i][j] == 'P')
+			{
+				game->player.x = j;
+				game->player.y = i;
+				game->map->map[i][j] = '0';
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
+}
